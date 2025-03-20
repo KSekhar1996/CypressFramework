46f35696f4d337c891074138813dc853917cea6f
Sekhar1996 <kasisekhar083@gmail.com>
2025-03-20 10:00:24 +0530

    configured reports

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,10 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: "yandw7",
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
-
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
     baseUrl: "https://naveenautomationlabs.com/opencart/index.php?route=account/login",
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}'
@@ -16,6 +17,7 @@ module.exports = defineConfig({
 
   video: true,
   screenshotOnRunFailure: true,
+  videoOnFailOnly: true,
   viewportWidth: 1920,
   viewportHeight: 1080,
   defaultCommandTimeout: 3500,
