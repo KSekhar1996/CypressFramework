195720d4c2bfac60731ba40427c905fe795a705d
Sekhar1996 <kasisekhar083@gmail.com>
2025-03-20 10:28:28 +0530

    configured grep tags

--- cypress.config.js
+++ cypress.config.js
@@ -6,6 +6,8 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
+      require('@cypress/grep/src/plugin')(config);
+      return config;
     },
     baseUrl: "https://naveenautomationlabs.com/opencart/index.php?route=account/login",
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}'
@@ -16,8 +18,8 @@ module.exports = defineConfig({
   },
 
   video: true,
-  screenshotOnRunFailure: true,
-  videoOnFailOnly: true,
+  screenshotOnRunFailure: false,
+  videoOnFailOnly: false,
   viewportWidth: 1920,
   viewportHeight: 1080,
   defaultCommandTimeout: 3500,
